b1bed1d71c9990243282e7cd69f3ca89f51ee1b0
David Afdal Kaizar Mutahadi <94096108+DavidAfdal@users.noreply.github.com>
2024-06-14 16:12:02 +0700

    update send email

--- utils/send_email.js
+++ utils/send_email.js
@@ -131,7 +131,7 @@ export const TemplateForResetEmail = (id) => {
     <p>Dear user,</p>
     <p>You have requested to reset your password. Click the button below to reset it:</p>
     
-    <a class="button" href='http://localhost:5000/forget-password/${id}'>Reset Password</a>
+    <a class="button" href='http://103.157.26.216:5000/forget-password/${id}'>Reset Password</a>
     
     <p>If you did not request a password reset, please ignore this email.</p>
     <p>Thank you!</p>
