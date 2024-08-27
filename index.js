e26604d37d7bbc1b68b660dc103469d1ced913b9
David Afdal Kaizar Mutahadi <94096108+DavidAfdal@users.noreply.github.com>
2024-08-27 12:37:52 +0700

    update

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 const PORT = 5000;
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser());
 app.use(cors({
   origin: 'https://www.kicksstore.site',
   methods: ['GET', 'POST', 'PUT', "PATCH", 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser());
 app.use('/api/shoe', Productroutes);
 app.use('/api/auth', AuthRoutes);
 app.use('/api/cart', CartRoutes);
